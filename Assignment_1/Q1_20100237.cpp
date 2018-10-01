6343424b708742d29228c2eaddbfb749582b110c
Asim Waheed <asim.waheed29@gmail.com>
2018-10-01 10:17:16 +0500

    Put parse function into graph.cpp
    The same function can be used for another questions as well since
    the input format is the same

--- Assignment_1/Q1_20100237.cpp
+++ Assignment_1/Q1_20100237.cpp
@@ -25,47 +25,6 @@ int ctoi(int c){
 	Overall Running Time Complexity: O(|V|+|E|).
 */
 
-// this function takes O(|V| + |E|) time since the time required
-	// to add an element in a set is O(1)
-Graph* parse_file(string filename){
-	string line;
-	ifstream f(filename);
-	if(f.is_open()){
-		getline(f, line);
-		// remove "n " and convert the rest to integer
-		int n = stoi(line.substr(2));
-
-		Graph* graph = new Graph(n);
-		while(getline(f, line)){
-			// find position of :
-			int end = line.find(":");
-
-			// find left side number
-			int chem1 = stoi(line.substr(0,end).substr(1));
-			// find right side numbers
-			string reacts = line.substr(end+1);
-
-			vector<string> tokens;
-			string token;
-			istringstream tokenStream(reacts);
-			// find vector of strings split across ','
-			// this takes O(N) time where N is length of string
-			// Also note: N is directly proportional to E which is
-					// the number of edges for a particular vertex
-			while(getline(tokenStream, token, ',')){
-				tokens.push_back(token);
-			}
-			for(vector<string>::iterator it = tokens.begin(); it != tokens.end(); ++it){
-				// Get full number in front of " C"
-				int chem2 = stoi((*it).substr(2));
-				insert_edge(graph, chem1, chem2);
-			}
-		}
-		return graph;
-	}
-	f.close();
-}
-
 // This function simply runs BFS, which is O(|V| + |E|) on any given node 
 	// and returns one of two things
 // 1. "OK" means that there is no odd-length cycle all vertices have been colors appropriately
--- graph.cpp
+++ graph.cpp
@@ -2,6 +2,7 @@
 #include <unordered_set>
 #include <fstream>
 #include <iterator>
+#include <sstream>
 
 using namespace std;
 // Using a graph structure with an array of unordered_sets
@@ -29,4 +30,45 @@ bool check_edge(const Graph* graph, int u, int v){
 	auto itr = graph->edges[u].find(v);
 
 	itr != graph->edges[u].end() ? true : false;
+}
+
+// this function takes O(|V| + |E|) time since the time required
+	// to add an element in a set is O(1)
+Graph* parse_file(string filename){
+	string line;
+	ifstream f(filename);
+	if(f.is_open()){
+		getline(f, line);
+		// remove "n " and convert the rest to integer
+		int n = stoi(line.substr(2));
+
+		Graph* graph = new Graph(n);
+		while(getline(f, line)){
+			// find position of :
+			int end = line.find(":");
+
+			// find left side number
+			int chem1 = stoi(line.substr(0,end).substr(1));
+			// find right side numbers
+			string reacts = line.substr(end+1);
+
+			vector<string> tokens;
+			string token;
+			istringstream tokenStream(reacts);
+			// find vector of strings split across ','
+			// this takes O(N) time where N is length of string
+			// Also note: N is directly proportional to E which is
+					// the number of edges for a particular vertex
+			while(getline(tokenStream, token, ',')){
+				tokens.push_back(token);
+			}
+			for(vector<string>::iterator it = tokens.begin(); it != tokens.end(); ++it){
+				// Get full number in front of " C"
+				int chem2 = stoi((*it).substr(2));
+				insert_edge(graph, chem1, chem2);
+			}
+		}
+		return graph;
+	}
+	f.close();
 }
